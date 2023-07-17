64123d2f94be6124bd20817376197bc8cca52a52
Manan Shah <manan.s.9999@gmail.com>
2023-07-18 00:41:39 +0530

    Made email as primary key

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const admin =  require('./src/config/firebase-config');
+const admin = require('./src/config/firebase-config');
 const { v4: uuidv4 } = require('uuid');
 const multer = require('multer');
 const path = require('path');
@@ -13,6 +13,28 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 
+
+app.use(function (req, res, next) {
+
+  //res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type,, Access-Control-Request-Method, Access-Control-Request-Headers");
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 // Firebase Authentication Middleware
 const authenticate = async (req, res, next) => {
   try {
@@ -22,6 +44,7 @@ const authenticate = async (req, res, next) => {
     }
     const token = authorizationHeader.split('Bearer ')[1].trim();
     const decodedToken = await admin.auth().verifyIdToken(token);
+    req.userEmail = decodedToken.email;
     req.userId = decodedToken.uid;
     next();
   } catch (error) {
@@ -46,41 +69,71 @@ const pool = new Pool(dbConfig);
 
 // Endpoint: Get Company by Company ID
 app.get('/company', authenticate, async (req, res) => {
-	try {
- 
-	  // Retrieve the company from the database
-	  const query = 'SELECT * FROM Companies WHERE company_firebase_id = $1';
-	  const result = await pool.query(query, [req.userId]);
-  
-	  // Check if the company exists
-	  if (result.rows.length === 0) {
-		return res.status(404).json({ error: 'Company not found' });
-	  }
-  
-	  const company = result.rows[0];
-	  res.status(200).json(company);
-	} catch (error) {
-	  console.error('Error retrieving company:', error);
-	  res.status(500).json({ error: 'Failed to retrieve the company' });
-	}
-  });
-  
+  try {
+    // Retrieve the company from the database
+    const query = 'SELECT * FROM Companies WHERE primary_email = $1';
+    const result = await pool.query(query, [req.userEmail]);
 
-  // Endpoint: Get All Templates of a Company
+    // Check if the company exists
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Company not found' });
+    }
+
+    const company = result.rows[0];
+    res.status(200).json(company);
+  } catch (error) {
+    console.error('Error retrieving company:', error);
+    res.status(500).json({ error: 'Failed to retrieve the company' });
+  }
+});
+
+// Endpoint: Get all Companies
+app.get('/companies', authenticate, async (req, res) => {
+  try {
+    const query = 'SELECT * FROM Companies';
+    const result = await pool.query(query);
+
+    const companies = result.rows;
+    res.status(200).json(companies)
+  }
+  catch (error) {
+    console.error('Error retrieving companies:', error);
+    res.status(500).json({ error: 'Failed to retrieve companies' });
+  }
+});
+
+// Endpoint: Delete a Company
+app.delete('/company/:company_id', authenticate, async (req, res) => {
+  try {
+    console.log(req.params.company_id);
+    // delete
+    let delete_status = await pool.query(
+      'DELETE FROM  Companies WHERE company_id = $1',
+      [req.params.company_id]
+    );
+
+    res.status(201).json({ 'delete_status': delete_status });
+  } catch (error) {
+    console.error('Error deleting company:', error);
+    res.status(500).json({ error: 'Failed to delete the company' });
+  }
+});
+
+// Endpoint: Get All Templates of a Company
 app.get('/company/templates', authenticate, async (req, res) => {
-	try {
+  try {
 
-	  // Retrieve the templates of the company from the database
-	  const query = 'SELECT * FROM MessageTemplates WHERE company_firebase_id = $1';
-	  const result = await pool.query(query, [req.userId]);
-  
-	  const templates = result.rows;
-	  res.status(200).json(templates);
-	} catch (error) {
-	  console.error('Error retrieving templates:', error);
-	  res.status(500).json({ error: 'Failed to retrieve templates' });
-	}
-  });
+    // Retrieve the templates of the company from the database
+    const query = 'SELECT * FROM MessageTemplates,Companies WHERE MessageTemplates.company_id = Companies.company_id and Companies.primary_email = $1';
+    const result = await pool.query(query, [req.userEmail]);
+
+    const templates = result.rows;
+    res.status(200).json(templates);
+  } catch (error) {
+    console.error('Error retrieving templates:', error);
+    res.status(500).json({ error: 'Failed to retrieve templates' });
+  }
+});
 
 // Endpoint: Create a Company
 app.post('/companies', authenticate, async (req, res) => {
@@ -92,7 +145,7 @@ app.post('/companies', authenticate, async (req, res) => {
     // Save the company details to the database
     await pool.query(
       'INSERT INTO Companies (company_id,company_firebase_id, name, description, primary_email, primary_phone_number,credits) VALUES ($1, $2, $3, $4, $5 , $6, $7)',
-      [company_id, req.userId, name, description, primaryEmail, primaryPhoneNumber,credits]
+      [company_id, req.userId, name, description, primaryEmail, primaryPhoneNumber, credits]
     );
 
     res.status(201).json({ company_id });
@@ -113,12 +166,12 @@ app.post('/company/buy-credits', authenticate, async (req, res) => {
     }
 
     // Update the credits for the company in the database
-    await pool.query(
-      'UPDATE Companies SET credits = credits + $1 WHERE company_firebase_id = $2',
-      [credits, req.userId]
+    let dbResult=await pool.query(
+      'UPDATE Companies SET credits = credits + $1 WHERE primary_email = $2',
+      [credits, req.userEmail]
     );
 
-    res.status(200).json({ success: true });
+    res.status(200).json({ 'rowsUpdated': dbResult.rowCount });
   } catch (error) {
     console.error('Error buying credits:', error);
     res.status(500).json({ error: 'Failed to buy credits' });
@@ -126,9 +179,9 @@ app.post('/company/buy-credits', authenticate, async (req, res) => {
 });
 
 // Endpoint: Create Message Template
-app.post('/companies/:company_id/message-templates', authenticate, async (req, res) => {
+app.post('/companies/message-templates', authenticate, async (req, res) => {
   try {
-    const { company_id } = req.params;
+    const company_id = '';
     const { template_name, message_template } = req.body;
 
     // Validate input
